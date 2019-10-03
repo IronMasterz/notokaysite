c48636a8c3ea2c3113ae50839d9fa8e2d11089c6
NotOkay <43324435+NotOkayGit@users.noreply.github.com>
2019-10-03 09:44:29 -0400

    Create mj.php

---
+++ mj.php
@@ -0,0 +1,152 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Monkey Jump</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,shrink-to-fit=no" />
+<link rel="apple-touch-icon" href="MonkeyJump.jpg">
+<meta name="apple-mobile-web-app-title" content="Monkey Jump">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<link href="https://dseffects.com/ig/MonkeyJump.jpg" rel="icon" sizes="192x192" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="mobile-web-app-capable" content="yes">
+<meta property="og:description" content="DS Effects - Monkey Jump"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://www.dseffects.com"/>
+<meta property="og:image" content="https://dseffects.com/ig/MonkeyJump.jpg"/>
+<meta property="og:site_name" content="DS Effects"/>
+<meta property="og:title" content="My Best Score: 0"/>
+<style>
+html, body {
+  height: 100%;
+}
+.loader {
+  position: absolute;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  top: 50%;
+  height: 40px;
+  width: 6px;
+  background-color: transparent;
+  -webkit-animation: paddles 0.75s ease-out infinite;
+  animation: paddles 0.75s ease-out infinite;
+  -webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+.loader:before {
+  content: "";
+  position: absolute;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  top: 15px;
+  width: 10px;
+  height: 10px;
+  background-color: #fff;
+  border-radius: 50%;
+  -webkit-animation: ballbounce 0.6s ease-out infinite;
+  animation: ballbounce 0.6s ease-out infinite;
+}
+@-webkit-keyframes paddles {
+   0% {
+    box-shadow: -25px -10px 0px #fff, 25px 10px 0px #fff;
+  }
+  50% {
+    box-shadow: -25px 8px 0px #fff, 25px -10px 0px #fff;
+  }
+  100% {
+    box-shadow: -25px -10px 0px #fff, 25px 10px 0px #fff;
+  }
+}
+@keyframes paddles {
+  0% {
+    box-shadow: -25px -10px 0px #fff, 25px 10px 0px #fff;
+  }
+  50% {
+    box-shadow: -25px 8px 0px #fff, 25px -10px 0px #fff;
+  }
+  100% {
+    box-shadow: -25px -10px 0px #fff, 25px 10px 0px #fff;
+  }
+}
+@-webkit-keyframes ballbounce {
+  0% {
+    -webkit-transform: translateX(-20px) scale(1, 1.2);
+  }
+  25% {
+    -webkit-transform: scale(1.2, 1);
+  }
+  50% {
+    -webkit-transform: translateX(15px) scale(1, 1.2);
+  }
+  75% {
+    -webkit-transform: scale(1.2, 1);
+  }
+  100% {
+    -webkit-transform: translateX(-20px);
+  }
+}
+@keyframes ballbounce {
+  0% {
+    -webkit-transform: translateX(-20px) scale(1, 1.2);
+    transform: translateX(-20px) scale(1, 1.2);
+  }
+  25% {
+    -webkit-transform: scale(1.2, 1);
+    transform: scale(1.2, 1);
+  }
+  50% {
+    -webkit-transform: translateX(15px) scale(1, 1.2);
+    transform: translateX(15px) scale(1, 1.2);
+  }
+  75% {
+    -webkit-transform: scale(1.2, 1);
+    transform: scale(1.2, 1);
+  }
+  100% {
+    -webkit-transform: translateX(-20px);
+    transform: translateX(-20px);
+  }
+}
+</style>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-12443735-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag("js", new Date());
+  gtag("config", "UA-12443735-1");
+</script>
+
+<script type="text/javascript">
+window.cookieconsent_options={"message":"This site uses cookies, also of third parties, to send you advertising and services in line with your preferences. By closing this banner, scrolling down this page or clicking on any element on the page you accept the use of cookies. If you want either to know more or to refuse consent to all or just some cookies, please ",
+"dismiss":"OK","learnMore":"click here","link":"https://www.dseffects.com/cookiepolicy.html","theme":"cookieconsent"};
+</script>
+<script type="text/javascript" src="../cookieconsent.js"></script>
+<script>
+if(location==null)location="https://www.dseffects.com";
+
+if ( top !== self ) top.location.replace( self.location.href );
+
+</script>
+
+
+</head>
+<body bgcolor="#000000" text="#ffffff" vlink="#0000ff" alink="#0000ff" link="#0000ff">
+
+
+</head>
+<body style="-webkit-user-select: none;" bgcolor="#000000" text="#bfffff" vlink="#008000" alink="#00ff00" link="#ffff00">
+<div id="llt" style="position:absolute; left: 0px;top: 60%;width:100%;z-index:1;height:100%;text-align:center;font-weight:bold;color:#ff7;">www.dseffects.com</div>
+<div id="ll" class="loader"></div>
+<script>var dsp=["www.dseffects.com",3,0,0,1,1,2,"WU9VUiBCRVNUIFNDT1JFOiA=","U0hBUkUgWU9VUiBCRVNUIFNDT1JFOiA=","VGhlIG5hbWUgbXVzdCBiZSBhdCBsZWFzdCAzIGNoYXJhY3RlcnMhDQpQbGVhc2UgaW5zZXJ0IG9ubHkgY2hhcmFjaGVycyBhbmQgbnVtYmVycyE=","UGxlYXNlIGluc2VydCB5b3VyIG5hbWU=","bmFtZQ==",1,1,"aHR0cHM6Ly9kc2VmZmVjdHMuY29t","","YWQudHh0P3Y9","udf23hh3r62srayd4",1,"aHR0cHM6Ly9kc2VmZmVjdHMuY29tL2lnLw==","LnBocD9jPQ==","d2hhdHNhcHA6Ly9zZW5kP3RleHQ9","aHR0cHM6Ly9kc2VmZmVjdHMuY29t","R0FNRSBPVkVS","U2NvcmU=",0,"","","VGltZQ==","T2ggTm8h","R3JlYXQ=","TGl2ZXM=","TW92ZXM=","RW5lcmd5",1,"PGZvbnQgY29sb3I9I2ZmZmZiZj5XQVJOSU5HITwvZm9udD4gV2hlbiB5b3Ugc2VuZCB0aGUgbGluayBvbiBXaGF0c0FwcCw8YnI+PGZvbnQgY29sb3I9I2ZmZmZiZj5XQUlUPC9mb250PiB1bnRpbCB0aGUgc2NvcmUgaW1hZ2UgYXBwZWFycy4=","IEJlc3QgU2NvcmU6IA==","RFMgRWZmZWN0cyAtIA==","d3d3LmRzZWZmZWN0cy5jb20="];function myshow(){}function mydisable(){}</script>
+
+<script src="MonkeyJump_x.js"></script>   
+</body>
+</html>    
